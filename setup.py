033662fe8334617902c5770984f3c6d669f29a78
jayakedia10 <97061281+jayakedia10@users.noreply.github.com>
2024-06-27 12:39:09 +0530

    Miscellaneous updated

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import find_packages,setup
 from typing import List
-file_path="C:\Users\jayak\deeplearning_project\requirements_dev.txt"
+
 HYPEN_E_DOT= '-e .'
+
 def get_requirements(file_path : str) -> List[str]:
     requirements=[]
-    with open("C:\Users\jayak\deeplearning_project\requirements_dev.txt") as file_obj:
+    with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("\n", "") for req in requirements]
+
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+
     return requirements
 
 setup(
@@ -19,6 +22,7 @@ author="Jaya Kedia",
 author_email="jayakedia10@gmail.com",
 #install_requires=[], #either we can fill requirements here manually or we can write a logic that'll fetch from requirements_dev.txt
 #logic based
-install_requires= get_requirements("C:\Users\jayak\deeplearning_project\requirements_dev.txt"),
-package=find_packages()
+install_requires= get_requirements("requirements_dev.txt"),
+packages=find_packages()
+
 )
